4d49e6183ef6ec7f83e1b8f64a9660239ace484e
Yuki N <lunarbase@outlook.com>
2018-10-30 02:33:58 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -18,9 +18,14 @@ def last_element(array)
   return array[array.length-1]
 end
 
-def first_element_with_array_methods
+def first_element_with_array_methods(array)
   return first_element(array)
 end
+
+def last_element_with_array_methods(array)
+  return last_element(array)
+end
+
 
 
 
