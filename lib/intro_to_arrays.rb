c2fb7dcd3325b405e43aa08accb024f6bf003fb3
Yuki N <lunarbase@outlook.com>
2018-10-30 02:30:10 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,22 @@
+def instantiate_new_array
+  return array = Array.new
+end
+
+def array_with_two_elements
+  return array = Array.new(2, "mimi")
+end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]  
+end
+
+def last_element(array)
+  return array[array.length-1]
+end
+
+
+  
